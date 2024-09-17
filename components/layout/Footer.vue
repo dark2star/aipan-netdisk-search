62e44426f82171e56baf2c257f342fc398221d64
红色暗星 <912425567@qq.com>
2024-09-16 21:36:17 -0500

    Revert "Product"

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row items-center justify-center my-[20px]">
     <p class="font-mono text-[12px] text-center text-gray-400">
-       DiskMall-网盘资源搜索
+       爱盼-网盘资源搜索
     </p>
 
   </div>
